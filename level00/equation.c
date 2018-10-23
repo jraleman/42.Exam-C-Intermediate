2551ec780c765d137ac3530b53b841371300135c
jraleman <jraleman@bendiburg.com>
2018-10-23 00:08:12 -0700

    Comment out equation.c main function test

--- level00/equation.c
+++ level00/equation.c
@@ -14,41 +14,50 @@
 
 static int	join_digits(int left_digit, int right_digit)
 {
-	return ((left_digit * 10) + right_digit);
+		return ((left_digit * 10) + right_digit);
 }
 
 static void print_values(int a, int b, int c)
 {
-	printf("A = %d, ", a);
-	printf("B = %d, ", b);
-	printf("C = %d", c);
-	printf("\n");
-	return ;
+		printf("A = %d, ", a);
+		printf("B = %d, ", b);
+		printf("C = %d", c);
+		printf("\n");
+		return ;
 }
 
-void equation(int n)
+void 				equation(int n)
 {
-	int	num_a = 0;
-	int	num_b = 0;
-	int	num_c = 0;
-	int	total_sum = 0;
+		int	num_a = 0;
+		int	num_b = 0;
+		int	num_c = 0;
+		int	total_sum = 0;
 
-	while (num_a < 10)
-	{
-		while (num_b < 10)
+		while (num_a < 10)
 		{
-			while (num_c < 10)
-			{
-				total_sum = join_digits(num_a, num_b) + join_digits(num_c, num_a);
-				if (total_sum == n)
-					print_values(num_a, num_b, num_c);
-				num_c += 1;
-			}
-			num_c = 0;
-			num_b += 1;
+				while (num_b < 10)
+				{
+						while (num_c < 10)
+						{
+								total_sum = join_digits(num_a, num_b) \
+														+ join_digits(num_c, num_a);
+								if (total_sum == n)
+										print_values(num_a, num_b, num_c);
+								num_c += 1;
+						}
+						num_c = 0;
+						num_b += 1;
+				}
+				num_b = 0;
+				num_a += 1;
 		}
-		num_b = 0;
-		num_a += 1;
-	}
-	return ;
+		return ;
 }
+
+/*
+int 				main(void)
+{
+	equation(42);
+	return (0);
+}
+*/
