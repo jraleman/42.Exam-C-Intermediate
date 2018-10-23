3e8e65bdfaf26f59527fe5f6f4ccfbb3369bf634
jraleman <jraleman@bendiburg.com>
2018-10-22 23:38:31 -0700

    Refactor count_of_2.c

--- level00/count_of_2.c
+++ level00/count_of_2.c
@@ -1,30 +1,40 @@
-// function to count number of two's in a particular number
-void	ft_counttwo(int nb, int *counter)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   count_of_2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/22 23:36:29 by jaleman           #+#    #+#             */
+/*   Updated: 2018/10/22 23:36:29 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void		count_number(int nb, int *counter)
 {
-	if (nb >= 10)
-	{
-		ft_counttwo(nb / 10, counter);
-		ft_counttwo(nb % 10, counter);
-	}
-	if (nb == 2)
-	{
-		*counter += 1;
-	}
+		if (nb >= 10)
+		{
+				count_number(nb / 10, counter);
+				count_number(nb % 10, counter);
+		}
+		if (nb == 2)
+				*counter += 1;
+		return ;
 }
 
-int	count_of_2(int nb)
+int			count_of_2(int nb)
 {
-	int i;
-	int counter;
-	
-	i = 2;
-	counter = 0;
-	if (nb <= 1)
-		return (0);
-	while (i <= nb)
-	{
-		ft_counttwo(i, &counter);
-		i++;
-	}
-	return (counter);
+		int i;
+		int counter;
+
+		i = 2;
+		counter = 0;
+		if (nb < 0)
+				return (0);
+		while (i <= nb)
+		{
+				count_number(i, &counter);
+				i += 1;
+		}
+		return (counter);
 }
